dc99c383be8ecbd476d38949143ad04be07681d9
Sanjana <savala01@bvsd.org>
2024-09-19 20:13:24 -0600

    Update Square.java with finished code

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,9 +1,10 @@
 /**
  * Implements a class that encapsulates data and methods for representing a square
- * @author 
+ * @Sanjana 
  * @version 1.0
- * @since 2024-09-
+ * @since 2024-09-19
  */
+import java.lang.Math;
 
 class Square {
         /** The side length of the square **/
@@ -13,15 +14,18 @@ class Square {
     private double perimeter = 4.0;
 
         /** The area of the square **/
-        // TODO: add an instance variable for area
-    
+    private double area = 1.0;
 
+    
     /**
      * Constructor
      * @param side double representing the side length. Assumes positive non-zero 
      */
-        // TODO: add a constructor that takes a side length
-
+    public Square(double s) {
+        side = s;
+        perimeter = 4 * side;
+        area = side * side ;
+    }
 
     /**
      * Gets side length
@@ -35,13 +39,17 @@ class Square {
      * Gets perimeter
      * @return double representing perimeter
      */
-    // TODO: implement getter for perimeter
+    public double getPerimeter() {
+        return perimeter;
+    }
 
     /**
      * Gets area
      * @return double representing area
      */
-    // TODO: implement getter for area
+    public double getArea() {
+        return area;
+    }
 
     /**
      * Sets side length. Perimeter and area are updated
@@ -50,18 +58,27 @@ class Square {
     public void setSide(double s) {
         side = s;
         perimeter = 4 * side; // calculate perimeter
-        // TODO: calculate the area
+        // calculate the area
+        area = side * side;
     }
 
     /**
      * Sets area. Perimeter and side length are updated
      */
-    // TODO: implement - you will need to import java.lang.Math to use sqrt() method
+    public void setArea(double a) {
+        area = a;
+        side = Math.sqrt(area);
+        perimeter = 4 * side;
+
+    }
 
 
     /**
      * Sets perimeter. Area and side length are updated
      */
-    // TODO: implement - you will need to import java.lang.Math to use sqrt() method
-
+    public void setPerimeter(double p) {
+        perimeter = p;
+        side = p / 4;
+        area = side * side;
+    }
 }
